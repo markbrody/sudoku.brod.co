b5513e0c4fac499ffced48047a303b3667a31044
Mark Brody <markbrody@users.noreply.github.com>
2019-12-28 15:55:30 -0600

    update analytics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -133,7 +133,7 @@
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'UA-51049739-3');
+            gtag('config', 'UA-51049739-4');
         </script>
     </body>
 <html>
